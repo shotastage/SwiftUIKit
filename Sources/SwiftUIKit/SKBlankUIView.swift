c7b450c372b2f58c2560b90a2fc0126e216ebd69
Shota Shimazu <hornet.live.mf@gmail.com>
2022-04-17 00:26:10 +0900

    add blank view

---
+++ Sources/SwiftUIKit/SKBlankUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SKBlankView.swift
+//  
+//
+//  Created by Shota Shimazu on 2022/04/16.
+//
+
+import SwiftUI
+
+struct SKBlankView: View {
+    var body: some View {
+        Text("__BLANK__")
+    }
+}
+
+struct SKBlankView_Previews: PreviewProvider {
+    static var previews: some View {
+        SKBlankView()
+    }
+}
