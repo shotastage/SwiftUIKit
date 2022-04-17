98018d474b4459707928aaa0509b30f583461c18
Shota Shimazu <hornet.live.mf@gmail.com>
2022-04-17 18:28:26 +0900

    add initial example app

--- Example/Example/ContentView.swift
+++ Example/Example/ContentView.swift
@@ -6,11 +6,28 @@
 //
 
 import SwiftUI
+import SwiftUIKit
+import MapKit
 
 struct ContentView: View {
+
+    @State private var isActive: Bool = true
+    
+    @State private var centerRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 139.63104, longitude: 35.46124), span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02))
+
+    @State private var showUserLocation = true
+
+    private let views = ["🗺 Map View", "🌏 Web View", "⬛️ Blank View"]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(views, id: \.self) { view in
+                NavigationLink(destination: Text("\(view)番目のView")) {
+                            Text(view)
+                        }
+            }
+            .navigationTitle("SwiftUIKit Views")
+        }
     }
 }
 
